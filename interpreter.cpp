f9b00e6a2d1a62646a9a771e19b04a53fd6d5ea0
Gonçalo Pereira <goncaloepifanio@gmail.com>
2017-03-04 17:46:41 +0000

    Updated interpreter
    Seems operational.
    Theoretically, deals with exceptions, converts strings to float and sends the arguments accordingly.
    May require some extra few tests to confirm the way it's dealing with the exceptions as C++ may be a bit -too- nitpicky.

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <fstream>
 #include <iostream>
 #include "generator.h"
@@ -12,19 +13,23 @@ int main(int argc, char* argv[]){
     exit(EXIT_FAILURE);
   }
 
-  else{
+  else {
     ofstream fp;
 
     if(!strcmp(argv[1],"plane")){
       if((argc == 5)){
-		// need to convert the arguments explicitly with stof() or the like !!
-        float length = argv[3], width = argv[4];
-        fp.open(argv[2], ios::trunc);
-        generatePlane(fp, length, width);
-        fp.close();
+        try{
+          float length = std::stof(argv[3]), width = std::stof(argv[4]);
+          fp.open(argv[2], ios::trunc);
+          generatePlane(fp, length, width);
+          fp.close();
+        }
+        catch(invalid_argument e){
+          cerr << "Invalid type of arguments provided.";
+          exit(EXIT_FAILURE);
+        }
       }
-      else
-      {
+      else {
 	cerr << "Invalid number of arguments provided.";
         exit(EXIT_FAILURE);
       }
@@ -32,50 +37,81 @@ int main(int argc, char* argv[]){
 
     else if(!strcmp(argv[1],"box")){
       if((argc == 6)){
-		 // need to convert the arguments explicitly with stof() or the like !!
-        float length = argv[3], height = argv[4], width = argv[5];
-        fp.open(argv[5], ios::trunc);
-        generateBox(fp, length, height, width, 0);
-        fp.close();
+        try{
+          float length = std::stof(argv[3]), height = std::stof(argv[4]);
+          float width = std::stof(argv[5]);
+          fp.open(argv[5], ios::trunc);
+          generateBox(fp, length, height, width, 0);
+          fp.close();
+        }
+        catch(invalid_argument e){
+          cerr << "Invalid type of arguments provided.";
+          exit(EXIT_FAILURE);
+        }
       }
       else if((argc == 7)){
-		// need to convert the arguments explicitly with stof() or the like !!
-        float length = argv[3], height = argv[4];
-        float width = argv[5], divisions = argv[6];
-        fp.open(argv[6], ios::trunc);
-        generateBox(fp, length, height, width, divisions)
-        fp.close();
+        try{
+          float length = std::stof(argv[3]), height = std::stof(argv[4]);
+          float width = std::stof(argv[5]), divisions = std::stof(argv[6]);
+          fp.open(argv[6], ios::trunc);
+          generateBox(fp, length, height, width, divisions)
+          fp.close();
+        }
+        catch(invalid_argument e){
+          cerr << "Invalid type of arguments provided.";
+          exit(EXIT_FAILURE);
+        }
       }
-      else cerr << ("Invalid number of arguments provided.");
-      exit(EXIT_FAILURE);
+      else {
+        cerr << ("Invalid number of arguments provided.");
+        exit(EXIT_FAILURE);
+      }
     }
 
     else if(!strcmp(argv[1],"sphere")){
       if((argc == 6)){
-		// need to convert the arguments explicitly with stof() or the like !!
-        float radius = argv[3], slices = argv[4], stacks = argv[5];
-        fp.open(argv[5], ios::trunc);
-        generateSphere(fp, radius, slices, stacks);
-        fp.close();
+        try{
+          float radius = std::stof(argv[3]), slices = std::stof(argv[4]);
+          float stacks = std::stof(argv[5]);
+          fp.open(argv[5], ios::trunc);
+          generateSphere(fp, radius, slices, stacks);
+          fp.close();
+        }
+        catch(invalid_argument e){
+          cerr << "Invalid type of arguments provided.";
+          exit(EXIT_FAILURE);
+        } 
       }
-      else cerr << ("Invalid number of arguments provided.");
-      exit(EXIT_FAILURE);
+      else {
+        cerr << ("Invalid number of arguments provided.");
+        exit(EXIT_FAILURE);
+      }
     }
 
     else if(!strcmp(argv[1],"cone")){
       if((argc == 7)){
-		// need to convert the arguments explicitly with stof() or the like !!
-        float radius = argv[3], height = argv[4];
-        float slices = argv[5], stacks = argv[6];
-        fp.open(argv[6], ios::trunc);
-        generateCone(fp, radius, height, slices, stacks);
-        fp.close();
+	try{
+          float radius = std::stof(argv[3]), height = std::stof(argv[4]);
+          float slices = std::stof(argv[5]), stacks = std::stof(argv[6]);
+          fp.open(argv[6], ios::trunc);
+          generateCone(fp, radius, height, slices, stacks);
+          fp.close();
+        }
+        catch(invalid_argument e){
+          cerr << "Invalid type of arguments provided.";
+          exit(EXIT_FAILURE);
+        }
       }
-      else cerr << ("Invalid number of arguments provided.");
+      else {
+        cerr << ("Invalid number of arguments provided.");
+        exit(EXIT_FAILURE);
+      }
+    }
+    
+    else {
+      cerr << ("Available graphical primitives: plane box sphere cone");
       exit(EXIT_FAILURE);
     }
-    else cerr << ("Available graphical primitives: plane box sphere cone");
-    exit(EXIT_FAILURE);
   }
   return 0;
 }

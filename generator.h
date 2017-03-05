814fd19dcd6482fbc7304d2e5a692b077d347fa1
To-Silva <a73827@alunos.uminho.pt>
2017-03-05 00:06:33 +0000

    namespace
    kill me

--- generator.h
+++ generator.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <fstream>
+#include <string>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-namespace generator 
-{
-	void generatePlane(ofstream fp, float length, float width);
-	void generateBox(ofstream fp, float length, float height, float width, int divisions);
-	void generateSphere(ofstream fp, float radius, int slices, int stacks);
-	void generateCone(ofstream fp, float radius, float height, int slices, int stacks);
-	void generateCylinder(ofstream fp,float radius,float height,int slices,int stacks); //optional
-}
+using namespace std;
+void generatePlane(ofstream fp, float length, float width);
+void generateBox(ofstream fp, float length, float height, float width, int divisions);
+void generateSphere(ofstream fp, float radius, int slices, int stacks);
+void generateCone(ofstream fp, float radius, float height, int slices, int stacks);
+void generateCylinder(ofstream fp,float radius,float height,int slices,int stacks); //optional
